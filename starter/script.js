bfab42ff2906146fc7dc09f1c9bb5c05d5c1a130
Chijioke Nwagwu <wyhzest@gmail.com>
2019-10-26 18:11:08 +0100

    adds more objects

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,17 @@
 let john = {
 	name: 'John',
 	yearOfBirth: 1990,
-	isMarried: false,
+	isMarried: false
+}
+
+let mary = {
+	name: 'Mary',
+	yearOfBirth: 1995,
+	isMarried: true
+}
+
+let jane ={
+	name: 'Jane',
+	yearOfBirth: 1992,
+	isMarried: false
 }
