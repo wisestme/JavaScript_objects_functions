8ec16b01c93992384498308dbdaac6aa7f03d5d5
Chijioke Nwagwu <wyhzest@gmail.com>
2019-11-01 08:08:53 +0100

    creates a constructor function

--- starter/script.js
+++ starter/script.js
@@ -2,16 +2,25 @@ let john = {
 	name: 'John',
 	yearOfBirth: 1990,
 	isMarried: false
-}
+};
 
 let mary = {
 	name: 'Mary',
 	yearOfBirth: 1995,
 	isMarried: true
-}
+};
 
 let jane ={
 	name: 'Jane',
 	yearOfBirth: 1992,
 	isMarried: false
+};
+
+const Person = function(name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
 }
+
+const john = new Person('John', 1990, 'teacher');
+const wisest = new Person('Wisest', 1987, 'Developer')
