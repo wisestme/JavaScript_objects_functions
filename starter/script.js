4105f1fd943baa7e563f20a25cba582f85b69326
Chijioke Nwagwu <wyhzest@gmail.com>
2019-11-05 16:11:23 +0100

    creates a method

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,5 @@
-let john = {
+// Object literals
+/*let john = {
 	name: 'John',
 	yearOfBirth: 1990,
 	isMarried: false
@@ -14,13 +15,23 @@ let jane ={
 	name: 'Jane',
 	yearOfBirth: 1992,
 	isMarried: false
-};
+};*/
 
+// Function constructor
+// Blueprint
 const Person = function(name, yearOfBirth, job) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
 	this.job = job;
+	console.log(this);
+	this.calculateAge = function() {
+		console.log(2019 - this.yearOfBirth);
+	}
 }
 
+// Instantiation
 const john = new Person('John', 1990, 'teacher');
+
 const wisest = new Person('Wisest', 1987, 'Developer')
+
+john.calculateAge();
